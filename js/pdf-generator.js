20e08acc07b15eb4f354e5c40d614d101c9a53ce
ericwinstanley-maker <eric.winstanley@goodcarbonco.com>
2026-04-11 00:18:16 -0400

    fix(pdf): revert material checkboxes to use pdf-lib native check method

--- js/pdf-generator.js
+++ js/pdf-generator.js
@@ -226,11 +226,7 @@ function fillSectionFields(form, inspection, photoRefs) {
       
       Object.keys(item1.selectedOptions || {}).forEach(opt => {
         if (item1.selectedOptions[opt] && optMap[opt]) {
-          try {
-            const f = form.getField(optMap[opt]);
-            f.acroField.dict.set(PDFName.of('V'), PDFName.of('Yes'));
-            f.acroField.getWidgets().forEach(w => w.setAppearanceState(PDFName.of('Yes')));
-          } catch(e) {}
+          trySetCheckbox(form, optMap[opt], true);
         }
       });
       
